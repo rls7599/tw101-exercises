8bcb587817238d5f222ddf05effd762f3ced9dd7
Ronnie Smith <rls8ru@virginia.edu>
2016-08-25 22:36:48 -0400

    completed exercise 7, look to optimize

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -4,9 +4,44 @@ package com.thoughtworks.tw101.exercises.exercise7;
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
+        Random r = new Random();
+
+        //0-99
+        int randomNumb = r.nextInt(100)+1;
+        Scanner inputReader = new Scanner(System.in);
+
+        while(true){
+
+
+            System.out.print("Pick a number between 1 and 100: ");
+            int userGuess = Integer.parseInt(inputReader.next());
+
+            if(userGuess == randomNumb){
+                System.out.println("You are correct! Exiting program.");
+                break;
+            }
+            //less than guess
+            else if(userGuess < randomNumb){
+                System.out.println("Try again! Your guess was too low.");
+                System.out.println();
+            }
+            //greater than guess
+            else{
+                System.out.println("Try again! Your guess was too high");
+                System.out.println();
+            }
+
+        }
+
+
 
     }
 }
